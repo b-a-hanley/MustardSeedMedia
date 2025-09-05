a4d44c14880b31bf409a5e7a00d69497f7379928
bahanley <benhanley321@gmail.com>
2025-09-05 22:18:24 +0100

    copy title

--- src/Views/ContactUs.php
+++ src/Views/ContactUs.php
@@ -4,7 +4,7 @@ require_once 'src/Views/Shared/Header.php';
 
 <div class="bg-section-background justify-center mx-6 my-6 rounded-xl sm:py-16">
   <div class="text-center px-6">
-    <h3 class="text-base font-semibold leading-7 text-primary">St Mary's Wollaton Park:</h3>
+    <h3 class="text-base font-semibold leading-7 text-primary">Saint Peter's Net:</h3>
     <p class="mt-2 text-4xl font-bold tracking-tight text-colour sm:text-5xl">Contact us</p>
   </div> 
 
