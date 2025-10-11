ac82ce1840d5bed6de1020e4e24c2fd00d6714f7
bahanley <benhanley321@gmail.com>
2025-10-11 15:35:49 +0100

    flavicon

--- src/Views/Shared/Header.php
+++ src/Views/Shared/Header.php
@@ -4,7 +4,7 @@
   <title>St Peter's Net</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="icon" type="image/png" href="/assets/Logo-brown.png">
+  <link rel="icon" type="image/x-icon" href="/assets/Logo-brown-flav.ico">
   <script src="https://cdn.tailwindcss.com"></script>
   <script src="https://cdn.jsdelivr.net/npm/flowbite@2.5.2/dist/flowbite.min.js"></script>
   <link rel="stylesheet" href="/assets/css/Colours.css">
